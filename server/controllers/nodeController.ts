01ed38378971abde565286becfa9a3fcd9a2f5b3
Solution_X_Desktop <xiongyu132@163.com>
2026-05-01 23:26:15 +0800

    fix: strip newline characters from node content in timeline concatenation

--- server/controllers/nodeController.ts
+++ server/controllers/nodeController.ts
@@ -165,7 +165,7 @@ export const getTimeline = (req: Request, res: Response) => {
     ORDER BY id ASC
   `, [story_id]) as any[];
 
-  const fullText = timelineNodes.map((n: any) => n.content).join('');
+  const fullText = timelineNodes.map((n: any) => n.content.replace(/[\r\n]/g, '')).join('');
 
   res.json({
     story_id: story.id,
